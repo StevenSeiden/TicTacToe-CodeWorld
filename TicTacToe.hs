baf8989c356ee37b7f09fe1f6cebde1b826566f8
Steven S <stevenrovello@gmail.com>
2018-11-04 17:03:00 -0600

    Randomizing the marks on the board.

--- TicTacToe.hs
+++ TicTacToe.hs
@@ -1,16 +1,31 @@
 import Extras((<$>))
+import System.Random
+--import Prelude()
+
 -- Modify this function, so that it draws any possible configuration of a board
 --draw(configuration) = text("Show the configuration here")
 
 -- Try different configurations to check that they show up correctly
+init(random) = random#1
 initial :: [Number] -> Board
-initial(_) = [ X, O, N
-             , N, X, N
-             , O, O, X ]
-             
-
-
-
+initial(random) = shuffled([ randomMark(1), randomMark(2), randomMark(3)
+                             , randomMark(4), randomMark(5), randomMark(6)
+                             , randomMark(7), randomMark(8), randomMark(9) ],random#1)
+   where
+   randomMark(number) =
+     if(random#number < 0.3) then X
+     else if(random#number < 0.6) then O
+     else N
+      
+  --where randomNum = newtype UniversalTime
+  --where [a, b, c, d, e, f] = first(randomNumbers(42), 6)
+  --randomNum :: Number
+  --where randomNum = first(randomNumbers([1,2],5))
+  --where first(initial(randomNumbers))
+  --where initial(rs) = (-8, rs # 1)
+  --lkdasjfkas :: [Number]
+  --lkdasjfkas = randomNumber(1)
+  
 
 draw :: Board -> Picture
 draw(state) = rectangle(-5,5)
@@ -23,20 +38,18 @@ draw(state) = rectangle(-5,5)
   & translated(rectangle(-5,5),0,-5)
   & translated(rectangle(-5,5),0,5)
   
-  --Upper row
-  & translated(scaled(text(findMark(state#1)),5,5),-5,5) --Left
-  & translated(scaled(text(findMark(state#2)),5,5),0,5)  --Center
-  & translated(scaled(text(findMark(state#3)),5,5),5,5)  --Right
+  & translated(scaled(text(findMark(state#1)),5,5),-5,5)
+  & translated(scaled(text(findMark(state#2)),5,5),0,5)
+  & translated(scaled(text(findMark(state#3)),5,5),5,5)
   
-  --Middle row
-  & translated(scaled(text(findMark(state#4)),5,5),-5,0) --Left
-  & translated(scaled(text(findMark(state#5)),5,5),0,0)  --Center
-  & translated(scaled(text(findMark(state#6)),5,5),5,0)  --Right
+  & translated(scaled(text(findMark(state#4)),5,5),-5,0)
+  & translated(scaled(text(findMark(state#5)),5,5),0,0)
+  & translated(scaled(text(findMark(state#6)),5,5),5,0)
 
-  --Lower row
-  & translated(scaled(text(findMark(state#7)),5,5),-5,-5)--Left
-  & translated(scaled(text(findMark(state#8)),5,5),0,-5) --Center
-  & translated(scaled(text(findMark(state#9)),5,5),5,-5) --Right
+  & translated(scaled(text(findMark(state#7)),5,5),-5,-5)
+  & translated(scaled(text(findMark(state#8)),5,5),0,-5)
+  & translated(scaled(text(findMark(state#9)),5,5),5,-5)
+
 
   
 
@@ -44,6 +57,11 @@ findMark :: Mark -> Text
 findMark(X) = "X"
 findMark(O) = "O"
 findMark(N) = ""
+{-findMark(currentState) =
+  if(currentState == X) then "X"
+  else if(currentState == O) then "O"
+  else ""-}
+
 --------------------------------------------------------------------------------
 -- DO NOT MODIFY THE CODE BELOW THIS LINE
 --------------------------------------------------------------------------------
