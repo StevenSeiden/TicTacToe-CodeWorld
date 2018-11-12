a2f1989a7b8819c6800f5537b9cee7823b57c4d9
Steven Seiden <stevenrovello@gmail.com>
2018-11-12 11:44:15 -0600

    Multiple users can place marks.

--- TicTacToe.hs
+++ TicTacToe.hs
@@ -1,33 +1,16 @@
 import Extras((<$>))
---import Prelude()
 
 -- Modify this function, so that it draws any possible configuration of a board
 --draw(configuration) = text("Show the configuration here")
 
 -- Try different configurations to check that they show up correctly
-init(random) = random#1
-initial :: [Number] -> Board
-initial(random) = shuffled([ randomMark(1), randomMark(2), randomMark(3)
-                             , randomMark(4), randomMark(5), randomMark(6)
-                             , randomMark(7), randomMark(8), randomMark(9) ],random#1)
-   where
-   randomMark(number) =
-     if(random#number < 0.3) then X
-     else if(random#number < 0.6) then O
-     else N
-      
-  --where randomNum = newtype UniversalTime
-  --where [a, b, c, d, e, f] = first(randomNumbers(42), 6)
-  --randomNum :: Number
-  --where randomNum = first(randomNumbers([1,2],5))
-  --where first(initial(randomNumbers))
-  --where initial(rs) = (-8, rs # 1)
-  --lkdasjfkas :: [Number]
-  --lkdasjfkas = randomNumber(1)
-  
+initial :: [Number] -> State
+initial(random) = ([ N, N, N
+                  , N, N, N
+                  , N, N, N ],PlayerO)
 
-draw :: Board -> Picture
-draw(state) = rectangle(-5,5)
+draw :: State -> Picture
+draw(board,player) = rectangle(-5,5)
   & translated(rectangle(-5,5),5,0)
   & translated(rectangle(-5,5),5,-5)
   & translated(rectangle(-5,5),5,5)
@@ -37,36 +20,62 @@ draw(state) = rectangle(-5,5)
   & translated(rectangle(-5,5),0,-5)
   & translated(rectangle(-5,5),0,5)
   
-  & pictures(placeCell <$> [0..8])
-  where
-  placeCell(cell) = translated(scaled(text(findMark(state#(cell+1))),5,5)
-                             , (-5*remainder(cell,3))+5
-                             , (5*truncation(cell/3))-5
-                             )
-
+  & translated(scaled(text(findMark(board#1)),5,5),-5,5)
+  & translated(scaled(text(findMark(board#2)),5,5),0,5)
+  & translated(scaled(text(findMark(board#3)),5,5),5,5)
   
+  & translated(scaled(text(findMark(board#4)),5,5),-5,0)
+  & translated(scaled(text(findMark(board#5)),5,5),0,0)
+  & translated(scaled(text(findMark(board#6)),5,5),5,0)
+
+  & translated(scaled(text(findMark(board#7)),5,5),-5,-5)
+  & translated(scaled(text(findMark(board#8)),5,5),0,-5)
+  & translated(scaled(text(findMark(board#9)),5,5),5,-5)
 
 findMark :: Mark -> Text
 findMark(X) = "X"
 findMark(O) = "O"
 findMark(N) = ""
-{-findMark(currentState) =
-  if(currentState == X) then "X"
-  else if(currentState == O) then "O"
-  else ""-}
 
---------------------------------------------------------------------------------
--- DO NOT MODIFY THE CODE BELOW THIS LINE
---------------------------------------------------------------------------------
+set :: (Number,a,[a]) -> [a]
+set(index,value,list) = change <$> [1..length(list)]
+  where
+  change(i) = if i == index then value else list#i
+
 
 type Board = [Mark]
+data Player = PlayerX | PlayerO
+type State = ([Mark],Player)
 
 data Mark = X | O | N
 
 main = interactionOf(initial,update,handle,draw)
 
-update :: (Board,Number) -> Board
+update :: (State,Number) -> State
 update(state,elapsedTime) = state
+changePlayer :: (Player) -> Player
+changePlayer(PlayerX) = PlayerO
+changePlayer(PlayerO) = PlayerX
 
-handle :: (Board,Event) -> Board
+markToPlayer :: Player -> Mark
+markToPlayer(PlayerX) = X
+markToPlayer(PlayerO) = O
+
+handle :: (State,Event) -> State
+handle((board,player),MousePress(LeftButton,(x,y))) =
+  if board#findClickedMark(x,y) == N then (set(findClickedMark(x,y),markToPlayer(player),board),changePlayer(player))
+  else (board,player)
+  
+  where
+  findClickedMark(x,y) = if y >= 2.5 && x>=2.5 then 3
+  else if y>=2.5 && x >= -2.5 then 2
+  else if y>=2.5 then 1
+  
+  else if y >= -2.5 && x >= 2.5 then 6
+  else if y >= -2.5 && x >= -2.5 then 5
+  else if y>= -2.5 then 4
+
+  else if x >= 2.5 then 9
+  else if x >= -2.5 then 8
+  else 7
 handle(state,event) = state
